ac59c88357c5dda0a71d908799d589859a4d1df1
Postoak <129154578+brittpostoak@users.noreply.github.com>
2023-07-11 11:22:16 -0500

    Update testprint.c

--- testprint.c
+++ testprint.c
@@ -1,39 +1,91 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...) {
-  int count = 0;
-  va_list args;
-  va_start(args, format);
-
-  while (*format) {
-    if (*format == '%') {
-      format++;
-      switch (*format) {
-        case 'c': {
-          char c = va_arg(args, int);
-          count += printf("%c", c);
-          break;
-        }
-        case 's': {
-          char *s = va_arg(args, char *);
-          count += printf("%s", s);
-          break;
-        }
-        case '%': {
-          count += printf("%%");
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    } else {
-      count += printf("%c", *format);
+int _printf(const char *format, ...)
+{
+    int count = 0;
+    va_list args;
+    if (format == NULL)
+    {
+	    return (-1);
     }
-    format++;
-  }
+    va_start(args, format);
 
-  va_end(args);
-  return count;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                {
+                    char c = va_arg(args, int);
+                    count += printf("%c", c);
+                    break;
+                }
+                case 's':
+                {
+                    char *s = va_arg(args, char *);
+                    count += printf("%s", s);
+                    break;
+                }
+                case '%':
+                {
+                    count += printf("%%");
+                    break;
+                }
+                case 'd':
+                case 'i':
+                {
+                    int num = va_arg(args, int);
+                    count += printf("%d", num);
+                    break;
+                }
+                case 'u':
+                {
+                    unsigned int num = va_arg(args, unsigned int);
+                    count += printf("%u", num);
+                    break;
+                }
+                case 'o':
+                {
+                    unsigned int num = va_arg(args, unsigned int);
+                    count += printf("%o", num);
+                    break;
+                }
+                case 'x':
+                {
+                    unsigned int num = va_arg(args, unsigned int);
+                    count += printf("%x", num);
+                    break;
+                }
+                case 'X':
+                {
+                    unsigned int num = va_arg(args, unsigned int);
+                    count += printf("%X", num);
+                    break;
+                }
+                case 'p':
+                {
+                    void *ptr = va_arg(args, void *);
+                    count += printf("%p", ptr);
+                    break;
+                }
+                default:
+                {
+                    count += printf("%%%c", *format);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            count += printf("%c", *format);
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
