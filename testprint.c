dda4c7d7668022ffe24fec0e9ec02427557fad3b
Postoak <129154578+brittpostoak@users.noreply.github.com>
2023-07-11 14:56:39 -0500

    Update testprint.c ?

--- testprint.c
+++ testprint.c
@@ -30,7 +30,7 @@ int _printf(const char *format, ...)
                 }
                 case '%':
                 {
-                    count += printf("%c", *format);
+                    count += printf("%%", *format);
                     break;
                 }
                 case 'd':
