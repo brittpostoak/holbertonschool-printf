dc77ad674d310b3b99df58cf387a6a79094a0afd
Brittany_Herman <brittherman86@gmail.com>
2023-07-11 09:03:02 -0700

    update

--- testprint.c
+++ testprint.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
-
 int _printf(const char *format, ...)
 {
     int count = 0;
     va_list args;
-
-    va_start(args, format);
-    if (format = NULL)
+  if (format == NULL)
     {
-	    return (-1);
-
+            return (-1);
+    }
+    va_start(args, format);
     while (*format)
     {
         if (*format == '%')
@@ -85,7 +83,6 @@ int _printf(const char *format, ...)
         }
         format++;
     }
-
     va_end(args);
     return count;
 }
